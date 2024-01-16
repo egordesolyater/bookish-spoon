81aebe66240c9f860f1f44c1cf66365cd3fe0b45
egordesolyater <147910281+egordesolyater@users.noreply.github.com>
2024-01-16 19:56:18 +0800

    Create n15.py

---
+++ n15.py
@@ -0,0 +1,28 @@
+# from numpy import *
+
+# # [1,2,3]
+# # [4,5,6]
+
+# arr=array([[1,2,3,''],[4,5,6,' ']])
+# print(arr.ndim)
+# print(arr.shape)
+# print(arr.size)
+# print(arange(10,30,5))
+# print(linspace(0,2,9))
+# arr2=arange(12).reshape(3,4)
+# print(arr2)
+
+# arr3=array([1,2,3])
+# arr4=array([3,2,1])
+# print(arr3+arr4)
+# print(arr3-arr4)
+# print(arr3**3)
+# print(list(arr3>=2))
+
+from numpy import *
+
+arr=array([51,41,31,21])
+# print(list(arr-array([1,1,1,1])))
+
+arr2=int(input())
+print(list(arr>=arr2))
